66d51e9e8f38908c924aa912f0aefe7c72b09631
S14mx <sergey.otryshko@gmail.com>
2022-01-03 20:32:24 -0800

    added dynamic result rendering with images, added styles

--- src/Results.js
+++ src/Results.js
@@ -9,8 +9,8 @@ class Results extends React.Component {
       <Container className="Results">
         {this.props.locationObj.map(({ display_name, lat, lon, place_id }) => (
           <Figure key={place_id}>
-            <Figure.Caption>{`Location: ${display_name}.`}<br/> {`Latitude: ${lat}.`}<br/> {`Longitude: ${lon}.`} </Figure.Caption>
-            <Figure.Image fluid src={''}/>
+            <Figure.Caption style={{ padding: '2rem'}}>{`Location: ${display_name}.`}<br/> {`Latitude: ${lat}.`}<br/> {`Longitude: ${lon}.`} </Figure.Caption>
+            <Figure.Image fluid style={{ boxShadow: '5px, 5px'}} src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${lat},${lon}&zoom=8}`} alt={display_name}/>
           </Figure>
         ))}
       </Container>
