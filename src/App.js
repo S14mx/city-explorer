78529301b8e7ac2c0fc22e89533cb2b454b22033
S14mx <sergey.otryshko@gmail.com>
2022-01-04 20:02:42 -0800

    formatted document

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ class App extends React.Component {
     this.state = {
       locationObj: [],
       error: ''
-    } 
+    }
   }
 
   getLocationObj = async (searchedCity) => {
     try {
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${searchedCity}&format=json`;
-    const response = await axios.get(url);
-    this.setState({ locationObj: response.data });
-    this.setState({ error: ''})
-    } catch(err) {
-      this.setState({ error: err.response.status + ': '+ err.response.data.error});
-  }
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${searchedCity}&format=json`;
+      const response = await axios.get(url);
+      this.setState({ locationObj: response.data });
+      this.setState({ error: '' })
+    } catch (err) {
+      this.setState({ error: err.response.status + ': ' + err.response.data.error });
+    }
   }
 
-  
+
   render() {
     const appStyle = {
       minHeight: '100vh',
@@ -38,13 +38,13 @@ class App extends React.Component {
     }
     return (
       <Container style={appStyle} className="App">
-        <SearchBar getLocationObj={this.getLocationObj}/>
+        <SearchBar getLocationObj={this.getLocationObj} />
         {this.state.error &&
-        <Alert style={alertStyle} variant='warning'>
-        {this.state.error}
-        </Alert>}
+          <Alert style={alertStyle} variant='warning'>
+            {this.state.error}
+          </Alert>}
         {this.state.locationObj.length > 0 &&
-        <Results locationObj={this.state.locationObj}/>}
+          <Results locationObj={this.state.locationObj} />}
       </Container>
 
 
